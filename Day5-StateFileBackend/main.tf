2a0722c4af538cf776255a8921333daa6f352c01
Mayank Gupta <mnkgpt12@gmail.com>
2025-07-26 10:49:47 +0530

    added

--- Day5-StateFileBackend/main.tf
+++ Day5-StateFileBackend/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "instance" {
   # ami = "ami-05ffe3c48a9991133" # This is the default AMI ID for us-east-1 
   instance_type = "t2.micro"
 
-  key_name = "key"
+
   # key_name = "test1" # This is the default key pair name
  tags = {
   Name = "Day5-stateBackend-instance" 
